cb02b0e149e454d992e2cd45b57c91c79cdd7be6
wendrew <wendrewdevelop@gmail.com>
2018-10-21 09:48:18 -0300

    alteração geral

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def index(request):
+    
+    return render(request, 'index.html')
